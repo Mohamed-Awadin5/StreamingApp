d1f6ecf62ced9dd69b7e875818cd7bac9dd19318
mohamed <mohamed.awadin6@gmail.com>
2026-04-25 19:18:22 +0300

    Upload full project correctly

---
+++ StreamingAppTests/StreamingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  StreamingAppTests.swift
+//  StreamingAppTests
+//
+//  Created by mohamed awadin7 on 20/04/2026.
+//
+
+import Testing
+@testable import StreamingApp
+
+struct StreamingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
